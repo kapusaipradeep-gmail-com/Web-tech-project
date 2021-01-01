c748241d9ca2d732cf868eb2765b67d73168f2db
saipradeepkapu <74823702+saipradeepkapu@users.noreply.github.com>
2021-01-01 12:23:40 +0530

    Add files via upload

---
+++ abc1.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$conn = mysqli_connect($servername, $username, $password);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$sql = "USE nitpy";
+$res=$conn->query($sql) ;
+$SUB_ID = test_input(strval($_GET["crs_id"]));
+$sql="update lock_table set lock_value=0 where COURSE_ID='".$SUB_ID."'";
+$res=$conn->query($sql) ;
+//echo "data base is locked";
+
+echo $SUB_ID;
+// echo 'Location:error.php?'.$SUB_ID;
+    header('Location:error.php?'.$SUB_ID) ;
+    function test_input($data) {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+    }
+    $conn->close();
+  //  echo "completed";
+
+ ?>
